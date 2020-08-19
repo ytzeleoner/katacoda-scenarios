ea94ee3967b47600abbea0263d4e62d5f997ff41
Pedro <ytzeleoner@gmail.com>
2020-08-19 10:46:23 +0200

    Otro init

--- hello-world/env-init.sh
+++ hello-world/env-init.sh
@@ -1,4 +1,8 @@
 mkdir prueba1
 cd prueba1
 touch newFile.txt
-cat newFile.txt < "echo Hola"
+echo "Hola" >> newFile.txt
+echo "Caracola" >> newFile.txt
+echo "Como mola" >> newFile.txt
+echo "Toda la ola" >> newFile.txt
+
